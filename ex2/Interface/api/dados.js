cdec58513684c1c6ca3214a8fa36b196b307298e
JoseBambora <zebragapt@gmail.com>
2023-05-30 16:04:56 +0100

    Esqueci-me de submeter estas pastas.

---
+++ ex2/Interface/api/dados.js
@@ -0,0 +1,4 @@
+module.exports.plantas = 'http://localhost:15030/plantas/'
+module.exports.planta = (id) => {return 'http://localhost:15030/plantas/'+id} 
+module.exports.especie = (id) => {return 'http://localhost:15030/plantas/?especie='+id} 
+
